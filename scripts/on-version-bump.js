e01e1b5d1fd69703840d81618764892958ba34aa
Nils Knappmeier <npm@knappi.org>
2024-05-25 17:10:31 +0200

    chore: fix version bump script

--- scripts/on-version-bump.js
+++ scripts/on-version-bump.js
@@ -2,6 +2,7 @@ import packageJson from "../package.json" assert { type: "json" };
 import cp from "child_process";
 import { editFile } from "./tools/editFile.js";
 
+cp.execSync("npm run build", { stdio: "inherit" });
 cp.execSync("npm run test", { stdio: "inherit" });
 
 await editFile("CHANGELOG.md", (contents) => {
@@ -18,4 +19,3 @@ Date: ${new Date().toISOString()}
 });
 
 cp.execSync("node ./scripts/build-readme.js", { stdio: "inherit" });
-cp.execSync("npm run build -ws", { stdio: "inherit" });
